e3d41d2ff0266d7f72d5fa3db2d6fd3c8f69e56d
wenguz <wenguz1518@gmail.com>
2018-10-18 21:58:19 -0400

    etiqueta partido
    Se agrego nombre a las etiquetas del form de registrar partido

--- Tenis/Project1/src/mypackage1/AltasJAction.java
+++ Tenis/Project1/src/mypackage1/AltasJAction.java
@@ -12,13 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AltasJAction extends Action 
 {
-  /**
-   * This is the main action called from the Struts framework.
-   * @param mapping The ActionMapping used to select this instance.
-   * @param form The optional ActionForm bean for this request.
-   * @param request The HTTP Request we are processing.
-   * @param response The HTTP Response we are processing.
-   */
+
   public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
   {
     return mapping.findForward("bien");
